3d22d110b2531aa36220ed96c41b34ff71ea2fc5
root <root@w251-virtualserver01.SchoolProject.cloud>
2020-01-25 17:13:37 +0000

    modified cloud files

--- homeworks/hw03/cloud/mqtt_broker_cloud_start.sh
+++ homeworks/hw03/cloud/mqtt_broker_cloud_start.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --name mosquitto --network w251hw03 -v "$PWD":/hw03 -p 1883:1883 -ti alpine_mosquitto sh
+docker run --rm --name mosquitto --network w251hw03 -v "$PWD":/hw03 -p 1883:1883 -ti alpine_mosquitto sh
