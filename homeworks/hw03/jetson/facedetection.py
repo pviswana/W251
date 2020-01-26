f28648fc4804305ddb13facee10a50cbef0bc733
Param Viswanathan <paramvis@gmail.com>
2020-01-26 01:08:16 -0800

    Update facedetection.py

--- homeworks/hw03/jetson/facedetection.py
+++ homeworks/hw03/jetson/facedetection.py
@@ -1,14 +1,10 @@
-#import numpy as np
 import cv2 as cv
 import paho.mqtt.client as mqtt
 import time
 
-face_counter = 0
-
 # 1 should correspond to /dev/video1 , your USB camera. The 0 is reserved for the TX2 onboard camera
 cap = cv.VideoCapture(1)
 haar_model = './haarcascade_frontalface_default.xml'
-#haar_model = '/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
 face_cascade = cv.CascadeClassifier(haar_model)
 
 LOCAL_MQTT_HOST="mosquitto"
@@ -43,23 +39,21 @@ while(True):
         face = gray[y:y+h, x:x+w]
         rc,png = cv.imencode('.png', face)
         msg = png.tobytes()
-        
-        msg_file = open('./data_output' + str(face_counter) + '.png', 'wb')
-        msg_file.write(msg)
-        msg_file.close()
-
+	
+	# Publish the message to the mqtt topic face_app
         local_mqttclient.publish(LOCAL_MQTT_TOPIC, msg, qos=1)
 
+	# Show the cropped face on the window frame
         cv.imshow('crop', face)
-        key = cv.waitKey(1000)
-        face_counter = face_counter + 1
+	# Wait for 10 seconds or until a key is pressed
+        key = cv.waitKey(10000)
 
-    # look fpr the escape key on the frame window
+    # look fpr the escape key on the frame window to exit
     if key == 27:
         break
 
-    time.sleep(1) 
-
 cap.release()
 cv.destroyAllWindows()
+
+# End the mqtt loop
 local_mqttclient.loop_stop()
